77434c9867457d7d24447b0a94ebdb6fde65bb46
grabrick <grabrick@mail.ru>
2022-09-06 21:19:28 +0600

    feat: working logic pagination

--- client/src/component/Friends/Friends.jsx
+++ client/src/component/Friends/Friends.jsx
@@ -3,7 +3,7 @@ import './Friends.css'
 import './Friends.scss'
 import FriendsRender from "./FriendsRender/FriendsRender";
 import { useDispatch, useSelector } from 'react-redux'
-import { renderFriendsActionCreator } from '../../redux/friendsReducer'
+import { renderFriendsActionCreator, changePageActionCreator } from '../../redux/friendsReducer'
 import { useEffect } from "react";
 import * as axios from 'axios'
 
@@ -13,9 +13,15 @@ function Friends() {
     // const requestURL = 'https://social-network.samuraijs.com/api/1.0/users';
     const dispatch = useDispatch();
     const users = useSelector((state) => state.friends.friendsList);
+
     const renderFriends = (user) => {
         dispatch(renderFriendsActionCreator(user));
     }
+
+    const changePage = (numberPage) => {
+      dispatch(changePageActionCreator(numberPage))
+    }
+
     const totalUsersCount = useSelector((state) => state.friends.totalUsersCount);
     const pagesSize = useSelector(state => state.friends.pagesSize);
     const currentPage = useSelector((state) => state.friends.currentPage);
@@ -32,11 +38,19 @@ function Friends() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    const pageCount = totalUsersCount / pagesSize;
+    const pageCount = Math.ceil(totalUsersCount / pagesSize)
 
     const pages = [];
     for (let i = 1; i <= pageCount; i++) {
       pages.push(i);
+    }
+
+    const onChangePage = (pageNumber) => {
+        changePage(pageNumber)
+          axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${pagesSize}`)
+              .then(res => {
+                  renderFriends(res.data.items);
+              })
     }
 
     return (
@@ -48,7 +62,8 @@ function Friends() {
 
           <div className="friends__main_pages">
             {pages.map((p) => {
-              return <span className={currentPage === p && activePage}>{p}</span>
+              return <span className={currentPage === p && activePage} id="number__page" onClick={() => {onChangePage(p)}}>{p}</span>
+                    
             })}
           </div>
         </div>
--- client/src/redux/friendsReducer.js
+++ client/src/redux/friendsReducer.js
@@ -1,6 +1,7 @@
 const RENDER_FRIENDS = 'RENDER-FRIENDS'
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
+const SET_CURRENT_PAGE = 'SET-CURRENT-PAGE'
 
 const initialState = {
     friendsList: [],
@@ -15,6 +16,12 @@ const friendsReducer = (state = initialState, action) => {
             return {
                 ...state, friendsList: action.friendsList
             }
+
+
+            case SET_CURRENT_PAGE:
+                return {
+                    ...state, currentPage: action.currentPage
+                }
         
         case FOLLOW: 
             return {
@@ -45,4 +52,5 @@ const friendsReducer = (state = initialState, action) => {
 export const followActionCreator = (userId) => ({type: FOLLOW, userId: userId})
 export const unFollowActionCreator = (userId) => ({type: UNFOLLOW, userId: userId})
 export const renderFriendsActionCreator = (friendsList) => ({type: RENDER_FRIENDS, friendsList: friendsList})
+export const changePageActionCreator = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage: currentPage})
 export default friendsReducer;
