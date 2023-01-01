2944d65c703a0b96532380aef5ba6438e4db0d9e
Rodion Strelkov <rodionstrelkov@Rodions-MacBook-Air.local>
2023-01-02 00:30:20 +0600

    feat: add Adaptive layout

--- client/src/component/Friends/Friends.tsx
+++ client/src/component/Friends/Friends.tsx
@@ -1,10 +1,9 @@
 import React, { FC } from "react";
-import './Friends.css'
-import './Friends.scss'
+import "./Friends.css";
+import "./Friends.scss";
 import FriendsRender from "./FriendsRender/FriendsRender";
 import Preloader from "../common/Preloader/preloader";
 import Pagination from "../common/Pagination/Pagination";
-
 
 const Friends: FC = () => {
   return (
@@ -16,16 +15,21 @@ const Friends: FC = () => {
             <p className="friends__main_header-text">Friends</p>
           </div>
           <div className="friends__main_friends">
-            <FriendsRender name={""} id={0} followed={false} photo={{
-              large: undefined,
-              small: undefined
-            }} />
+            <FriendsRender
+              name={""}
+              id={0}
+              followed={false}
+              photo={{
+                large: undefined,
+                small: undefined,
+              }}
+            />
           </div>
           <Pagination />
         </div>
       </section>
     </>
   );
-}
+};
 
 export default Friends;
